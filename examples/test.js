348c9e6a26355a1270a7c79814de45ef1237915c
ayush sharma <ayush.aceit@gmail.com>
2016-03-25 04:38:38 +0530

    test.js

---
+++ examples/test.js
@@ -0,0 +1,18 @@
+var app = require("diseasetargetviz");
+
+var config  = {
+	"dom":{
+			"viz":"rootDiv",
+			"filter":"filter",
+			"pathway":"pathway"
+		},
+	"disease" :[
+		{"name":"Familial cold urticaria","code":"Orphanet_47045"},
+		{"name":"Asthma","code":"EFO_0000270"},
+		{"name":"Heart Failure","code":"EFO_0003144"},
+		{"name":"Blood Pressure","code":"EFO_0004325"},
+	],
+	"datatypes":[]
+}
+
+app(config);
